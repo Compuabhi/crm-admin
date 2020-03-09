81d72e15f410996b81f8ca545c0536be50a3b206
compuabhi <abhiiitkgp93@gmail.com>
2020-03-09 20:26:48 +0530

    remove react code to clean backend code

--- static/src/components/Home/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Home = () =>
-    <section>
-        <div className="container text-center">
-            <h1>Welcome to EVOLV Family</h1>
-            <h3>One stop Solution for Your Gym</h3>
-        </div>
-    </section>;
